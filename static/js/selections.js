e505af28bf974990bc42003a6f03f268df336c07
callum <callumery11@outlook.com>
2025-08-02 15:49:06 +0100

    setting up frontend

--- static/js/selections.js
+++ static/js/selections.js
@@ -1,3 +1,26 @@
-function selections(){
-    console.log("Hello")
+let isEnabled = false;
+
+function selections() {
+    isEnabled = true;
+}
+
+function onClickFunction(event) {
+    console.log("Fired")
+    if (!isEnabled) {
+        return;
+    }
+
+    const cursorX = event.pageX; //get cursorX relative to whole page.
+    const cursorY = event.pageY; //get cursorY relative to whole page.
+
+    console.log(cursorX)
+    console.log(cursorY)
+}
+
+export function getPosition(element) {
+    const clientRect = element.getBoundingClientRect();
+    return {
+        left: clientRect.left + window.pageXOffset,
+        top: clientRect.top + window.pageYOffset
+    };
 }
