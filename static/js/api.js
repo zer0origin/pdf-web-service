dd2b8851edd7e872d8096a529fb83b599e5c68db
Callum <callumery11@outlook.com>
2026-01-18 00:02:16 +0000

    Add a new api.js script to handle fetch requests to the backend that are not using htmx

---
+++ static/js/api.js
@@ -0,0 +1,67 @@
+class SelectionDTO {
+    documentUUID
+    coordinates
+    pageKey
+
+    constructor(uuid, bounds, key) {
+        this.documentUUID = uuid
+        this.coordinates = bounds
+        this.pageKey = key
+    }
+}
+
+var apiModule = (function (){
+    /**
+     *
+     * @param data {Map<string, Array<Rectangle>>}
+     */
+    function convertSelectionMapToDTO(data ){
+        mapDTO = []
+        data.forEach((pageRectangleArray, key) => {
+            pageRectangleArray.forEach((rectangle) => {
+                p1 = rectangle.p1
+                p2 = rectangle.p2
+
+                if (!p1 || !p2){
+                    return
+                }
+
+                let name = String(rectangle.imageDiv.id);
+                let key = name.split("-")[1];
+
+                let selectionDTO = new SelectionDTO(getDocumentId(), {X1: p1.x, Y1: p1.y, X2: p2.x, Y2: p2.y}, key)
+                mapDTO.push(selectionDTO)
+            })
+        })
+
+        return mapDTO
+    }
+
+    /**
+     *
+     * @param data {Map<string, Array<Rectangle>>}
+     */
+    function saveSelectionsToDatabase(data){
+        let url = "/selection/bulk/"
+        let promise = fetch(url, {
+            method: "POST",
+            cache: "no-cache",
+            body: JSON.stringify(data)
+        })
+
+        promise.catch(reason => console.error(reason))
+    }
+
+    /**
+     * @returns {string}
+     */
+    function getDocumentId(){
+        return document.getElementById("viewer").attributes["documentId"].nodeValue;
+    }
+
+    return {
+        getDocumentId: getDocumentId,
+        convertSelectionMapToDTO: convertSelectionMapToDTO,
+        saveSelectionsToDatabase: saveSelectionsToDatabase
+    }
+})()
--- static/js/selections.js
+++ static/js/selections.js
@@ -1,160 +1,171 @@
+class Point {
+    x;
+    y;
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    copy() {
+        return new Point(this.x, this.y)
+    }
+
+    static getPositionRelativeToDocument(element) {
+        const clientRect = element.getBoundingClientRect();
+        return {
+            left: clientRect.left + window.pageXOffset,
+            top: clientRect.top + window.pageYOffset
+        };
+    }
+}
+
+class Rectangle {
+    static lastId = 0
+    /**
+     * @type {Point}
+     */
+    p1;
+    /**
+     * @type {Point}
+     */
+    p2;
+    /**
+     * @type {HTMLDivElement}
+     */
+    spawnDiv;
+    /**
+     * @type {HTMLDivElement}
+     */
+    imageDiv;
+    id;
+
+    /**
+     * @type {[HTMLElement]}
+     */
+    nodes = [];
+
+    /**
+     * @type {HTMLElement}
+     */
+    rectangleDiv = undefined
+
+    constructor(spawnDiv, imageDiv) {
+        this.spawnDiv = spawnDiv;
+        this.imageDiv = imageDiv;
+        this.id = Rectangle.lastId++;
+    }
+
+    /**
+     * Spawn a new point on the screen
+     * @param p {Point}
+     * @param element {HTMLElement}
+     * @param position {{left: number, top: number}}
+     */
+    #spawnPoint(p, element, position) {
+        let size = 5;
+        let temp = document.getElementById("point-template")
+        let node = document.importNode(temp.content.querySelector("div"), true)
+        element.appendChild(node)
+        this.nodes.push(node)
+
+        node.style.width = `${size}px`;
+        node.style.height = `${size}px`;
+        node.style.top = `${(position.top + (p.y * zoomModule.getZoomLevel())) - (size / 2)}px`
+        node.style.left = `${(position.left + (p.x * zoomModule.getZoomLevel())) - (size / 2)}px`
+    }
+
+    /**
+     * Spawn a new point on the screen
+     */
+    spawnP1() {
+        this.#spawnPoint(this.p1, this.spawnDiv, Point.getPositionRelativeToDocument(this.imageDiv));
+    }
+
+    /**
+     * Spawn a new point on the screen
+     */
+    spawnP2() {
+        this.#spawnPoint(this.p2, this.spawnDiv, Point.getPositionRelativeToDocument(this.imageDiv));
+    }
+
+    /**
+     * Remove all the spawned points. Points are visual elements on the screen, that have yet to become rectangles.
+     */
+    clearSpawnedPoints() {
+        while (this.nodes.length > 0) {
+            let element = this.nodes.pop();
+            element.remove();
+        }
+    }
+
+    /**
+     * Remove spawned rectangles.
+     */
+    clearSpawnedRectangle() {
+        if (this.rectangleDiv) {
+            this.rectangleDiv.remove()
+        }
+    }
+
+    /**
+     * Spawn a new point on the screen
+     */
+    spawnRectangle() {
+        let temp = document.getElementById("rectangle-template")
+        let node = document.importNode(temp.content.querySelector("div"), true)
+        this.rectangleDiv = node
+
+        let points = this.#getPointsAsUpperLeftAndLowerRight(this.p1, this.p2)
+        let width = (points.p2.x - points.p1.x) * zoomModule.getZoomLevel()
+        let height = (points.p2.y - points.p1.y) * zoomModule.getZoomLevel()
+
+        node.style.width = `${width}px`;
+        node.style.height = `${height}px`;
+        let position = Point.getPositionRelativeToDocument(this.imageDiv);
+        node.style.top = `${position.top + (points.p1.y * zoomModule.getZoomLevel())}px`
+        node.style.left = `${position.left + (points.p1.x * zoomModule.getZoomLevel())}px`
+
+        let exitControls = node.querySelector(".exit-controls")
+
+        exitControls.onclick = () => {
+            let name = String(this.imageDiv.id);
+            let key = name.split("-")[1];
+            selectionsModule.deleteSelection(key, this.id)
+        }
+
+        this.spawnDiv.appendChild(node)
+    }
+
+    /**
+     * Determines the top-left corner by subtracting half the width and height from the center point. It finds the
+     * bottom-right corner by adding half the width and height to that same center point
+     * @param p1 {Point}
+     * @param p2 {Point}
+     */
+    #getPointsAsUpperLeftAndLowerRight(p1, p2) {
+        let midX = (p1.x + p2.x) / 2;
+        let midY = (p1.y + p2.y) / 2;
+        let diffX = Math.abs(p1.x - p2.x) / 2;
+        let diffY = Math.abs(p1.y - p2.y) / 2
+
+        let upperLeft = new Point(midX - diffX, midY - diffY)
+        let lowerRight = new Point(midX + diffX, midY + diffY)
+
+        return {p1: upperLeft, p2: lowerRight}
+    }
+
+    clearSpawnedNodes() {
+        this.clearSpawnedPoints();
+        this.clearSpawnedRectangle();
+    }
+}
 var selectionsModule = (function () {
     Array.prototype.remove = function (from, to) {
         var rest = this.slice((to || from) + 1 || this.length);
         this.length = from < 0 ? this.length + from : from;
         return this.push.apply(this, rest);
     };
-
-    class Point {
-        x;
-        y;
-
-        constructor(x, y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        copy() {
-            return new Point(this.x, this.y)
-        }
-
-        static getPositionRelativeToDocument(element) {
-            const clientRect = element.getBoundingClientRect();
-            return {
-                left: clientRect.left + window.pageXOffset,
-                top: clientRect.top + window.pageYOffset
-            };
-        }
-    }
-
-    class Rectangle {
-        static lastId = 0
-        p1;
-        p2;
-        spawnDiv;
-        imageDiv;
-        id;
-
-        /**
-         * @type {[HTMLElement]}
-         */
-        nodes = [];
-
-        /**
-         * @type {HTMLElement}
-         */
-        rectangleDiv = undefined
-
-        constructor(spawnDiv, imageDiv) {
-            this.spawnDiv = spawnDiv;
-            this.imageDiv = imageDiv;
-            this.id = Rectangle.lastId++;
-        }
-
-        /**
-         * Spawn a new point on the screen
-         * @param p {Point}
-         * @param element {HTMLElement}
-         * @param position {{left: number, top: number}}
-         */
-        #spawnPoint(p, element, position) {
-            let size = 5;
-            let temp = document.getElementById("point-template")
-            let node = document.importNode(temp.content.querySelector("div"), true)
-            element.appendChild(node)
-            this.nodes.push(node)
-
-            node.style.width = `${size}px`;
-            node.style.height = `${size}px`;
-            node.style.top = `${(position.top + (p.y * zoomModule.getZoomLevel())) - (size / 2)}px`
-            node.style.left = `${(position.left + (p.x * zoomModule.getZoomLevel())) - (size / 2)}px`
-        }
-
-        /**
-         * Spawn a new point on the screen
-         */
-        spawnP1() {
-            this.#spawnPoint(this.p1, this.spawnDiv, Point.getPositionRelativeToDocument(this.imageDiv));
-        }
-
-        /**
-         * Spawn a new point on the screen
-         */
-        spawnP2() {
-            this.#spawnPoint(this.p2, this.spawnDiv, Point.getPositionRelativeToDocument(this.imageDiv));
-        }
-
-        /**
-         * Remove all the spawned points. Points are visual elements on the screen, that have yet to become rectangles.
-         */
-        clearSpawnedPoints() {
-            while (this.nodes.length > 0) {
-                let element = this.nodes.pop();
-                element.remove();
-            }
-        }
-
-        /**
-         * Remove spawned rectangles.
-         */
-        clearSpawnedRectangle() {
-            if (this.rectangleDiv) {
-                this.rectangleDiv.remove()
-            }
-        }
-
-        /**
-         * Spawn a new point on the screen
-         */
-        spawnRectangle() {
-            let temp = document.getElementById("rectangle-template")
-            let node = document.importNode(temp.content.querySelector("div"), true)
-            this.rectangleDiv = node
-
-            let points = this.#getPointsAsUpperLeftAndLowerRight(this.p1, this.p2)
-            let width = (points.p2.x - points.p1.x) * zoomModule.getZoomLevel()
-            let height = (points.p2.y - points.p1.y) * zoomModule.getZoomLevel()
-
-            node.style.width = `${width}px`;
-            node.style.height = `${height}px`;
-            let position = Point.getPositionRelativeToDocument(this.imageDiv);
-            node.style.top = `${position.top + (points.p1.y * zoomModule.getZoomLevel())}px`
-            node.style.left = `${position.left + (points.p1.x * zoomModule.getZoomLevel())}px`
-
-            let exitControls = node.querySelector(".exit-controls")
-
-            exitControls.onclick = () => {
-                let name = String(this.imageDiv.id);
-                let key = name.split("-")[1]; //todo: change
-                selectionsModule.deleteSelection(key, this.id)
-            }
-
-            this.spawnDiv.appendChild(node)
-        }
-
-        /**
-         * Determines the top-left corner by subtracting half the width and height from the center point. It finds the
-         * bottom-right corner by adding half the width and height to that same center point
-         * @param p1 {Point}
-         * @param p2 {Point}
-         */
-        #getPointsAsUpperLeftAndLowerRight(p1, p2) {
-            let midX = (p1.x + p2.x) / 2;
-            let midY = (p1.y + p2.y) / 2;
-            let diffX = Math.abs(p1.x - p2.x) / 2;
-            let diffY = Math.abs(p1.y - p2.y) / 2
-
-            let upperLeft = new Point(midX - diffX, midY - diffY)
-            let lowerRight = new Point(midX + diffX, midY + diffY)
-
-            return {p1: upperLeft, p2: lowerRight}
-        }
-
-        clearSpawnedNodes() {
-            this.clearSpawnedPoints();
-            this.clearSpawnedRectangle();
-        }
-    }
 
     /**
      *
@@ -183,15 +194,15 @@ var selectionsModule = (function () {
         }
 
         let name = String(event.target.id);
-        let pageNumber = name.split("-")[1];
-        let selectionArr = `selection-${pageNumber}`
+        let pageKey = name.split("-")[1];
+        let selectionArr = `selection-${pageKey}`
 
-        let present = selectionsMap.has(pageNumber)
+        let present = selectionsMap.has(pageKey)
         if (!present) {
-            selectionsMap.set(pageNumber, [])
+            selectionsMap.set(pageKey, [])
         }
 
-        let recArr = selectionsMap.get(pageNumber);
+        let recArr = selectionsMap.get(pageKey);
         if (recArr.length <= 0) {
             let rec = new Rectangle(document.getElementById(selectionArr), event.target);
             rec.p1 = imageCoordsRelativeToSelf;
@@ -212,6 +223,13 @@ var selectionsModule = (function () {
             recArr.push(rec)
             rec.spawnP1()
         }
+    }
+
+    /**
+     * @param data {SelectionDTO}
+     */
+    function load(data){
+
     }
 
     function deleteSelection(key, id) {
@@ -252,9 +270,9 @@ var selectionsModule = (function () {
 
 window.addEventListener("resize", selectionsModule.refreshSelectionNodes);
 
-if (window.zoomModule){
+if (window.zoomModule) {
     zoomModule.registerZoomChange(selectionsModule.refreshSelectionNodes);
-}else{
+} else {
     window.addEventListener("zoomModuleLoaded", () => {
         console.log("Event Fired!")
         zoomModule.registerZoomChange(selectionsModule.refreshSelectionNodes);
